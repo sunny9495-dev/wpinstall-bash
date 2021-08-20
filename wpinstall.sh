9aa43693a5b66a28b35be9b0a56cb0550fde45b4
sunny9495-dev <85738853+sunny9495-dev@users.noreply.github.com>
2021-08-20 18:30:04 +0530

    Update V1.5

--- wpinstall.sh
+++ wpinstall.sh
@@ -1,62 +1,62 @@
 #!/bin/bash
 cd ~/Desktop
-sudo apt-get install --fix-missing
-sudo apt update
-sudo apt-get install libc-dev-bin
-sudo apt-get install linux-libc-dev
-sudo apt-get instal libcrypt-dev
-sudo apt-get install libc6-dev
-sudo apt-get install libc-dev
-sudo apt-get install uuid-dev
-sudo apt-get install libpcre2-16-0
-sudo apt-get install libcre2-posix2
-sudo apt-get install gcc
-sudo apt-get install g++
-sudo apt-get install make
-sudo apt-get install dpkg-dev
-sudo apt-get install libpython3-dev
-sudo apt-get install python3.8-dev
-sudo apt-get install python3-lib2to3
-sudo apt-get instal python3-distutils
-sudo apt-get install libc6-dev
-sudo apt-get install libblkid-dev
-sudo apt-get install libpcre16-3
-sudo apt-get install libpcre32-3
-sudo apt-get install libpcrecpp0v5
-sudo apt-get install libsepol1-dev
-sudo apt-get install libpcre2-dev
-sudo apt-get install build-essential
-sudo apt-get install python3-dev
-sudo apt-get install libffi-dev
-sudo apt-get install libglib2.0-dev-bin
-sudo apt-get install libmount-dev
-sudo apt-get install libpcre3-dev
-sudo apt-get install libselinux1-dev
-sudo apt-get install zliblg-dev
-sudo apt-get install gconf2-common
-sudo apt-get install python3-lib2to3
-sudo apt-get install python3-setuptools
-sudo apt-get install python3-wheel
-sudo apt-get install python-pip-whl
-sudo apt-get install python3-pip
-sudo apt install python3-pip
-sudo apt-get install xclip
-pip3 install imap-tools --upgrade
-pip3 install selenium --upgrade
-pip3 install requests --upgrade
-pip3 install lxml --upgrade
-pip3 install pyperclip --upgrade
-pip3 install selenium-wire --upgrade
-sudo apt-get install libglib2.0-dev
-sudo apt-get install -y libgconf-2-4
-sudo apt-get install libnss3
-sudo apt-get install libfontconfig1
-sudo apt-get install libxss1
-sudo apt-get install unzip
-sudo apt-get install chromium-chromedriver
+apt install -y --fix-missing
+apt update
+apt upgrade
+apt install -y libc-dev-bin
+apt install -y linux-libc-dev
+apt install -y libcrypt-dev
+apt install -y libc6-dev
+apt install -y libc-dev
+apt install -y uuid-dev
+apt install -y libpcre2-16-0
+apt install -y gcc
+apt install -y g++
+apt install -y make
+apt install -y dpkg-dev
+apt install -y libpython3-dev
+apt install -y python3.8-dev
+apt install -y python3-lib2to3
+apt install -y python3-distutils
+apt install -y libc6-dev
+apt install -y libblkid-dev
+apt install -y libpcre16-3
+apt install -y libpcre32-3
+apt install -y libpcrecpp0v5
+apt install -y libsepol1-dev
+apt install -y libpcre2-dev
+apt install -y build-essential
+apt install -y python3-dev
+apt install -y libffi-dev
+apt install -y libglib2.0-dev-bin
+apt install -y libmount-dev
+apt install -y libpcre3-dev
+apt install -y libselinux1-dev
+apt install -y zliblg-dev
+apt install -y gconf2-common
+apt install -y python3-lib2to3
+apt install -y python3-setuptools
+apt install -y python3-wheel
+apt install -y python-pip-whl
+apt install -y python3-pip
+apt install -y python3-pip
+apt install -y xclip
+pip3 install -y imap-tools --upgrade
+pip3 install -y selenium --upgrade
+pip3 install -y requests --upgrade
+pip3 install -y lxml --upgrade
+pip3 install -y pyperclip --upgrade
+pip3 install -y selenium-wire --upgrade
+apt install -y libglib2.0-dev
+apt install -y -y libgconf-2-4
+apt install -y libnss3
+apt install -y libfontconfig1
+apt install -y libxss1
+apt install -y unzip
+apt install -y chromium-chromedriver
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
 wget -O wordpress.zip http://github.com/sunny9495-dev/linux-bash/blob/main/wordpress.zip?raw=true
 unzip wordpress.zip
